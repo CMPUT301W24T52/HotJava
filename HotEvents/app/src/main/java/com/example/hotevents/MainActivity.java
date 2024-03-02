f1ab021ad6f09f3a26008cf284a8f46440792f70
Bryan Kostelyk <bkostely@ualberta.ca>
2024-03-02 15:36:10 -0700

    Hot Events source template test

---
+++ HotEvents/app/src/main/java/com/example/hotevents/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hotevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button menuButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
