5a3a340c72485990cb8eec52ecebe18ff9a31326
Bryan Kostelyk <bkostely@ualberta.ca>
2024-03-04 18:55:50 -0700

    Added Event Obj. Added MyEvents Adapter and UI

--- HotEvents/app/src/main/java/com/example/hotevents/MainActivity.java
+++ HotEvents/app/src/main/java/com/example/hotevents/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.hotevents;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.google.firebase.firestore.CollectionReference;
@@ -16,12 +18,21 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+/**
+ * Home Page and control center of program
+ * TODO:
+ * - Change Menu button to Hamburger Icon
+ * - Provide endpoints to integrate other components
+ * - Make Event Icons functional
+ * - [Optional] Refresh Functionality
+ */
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
-
+    private CollectionReference eventsRef;
     MyEventsAdapter myEventsAdapter;
-    ArrayList<Event> eventArrayList;
+    ArrayList<Event> eventDataArray;
+    ListView eventList;
     Button menuButton;
 
 
@@ -31,7 +42,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        db = FirebaseFireStore.getIntstance()
+        db = FirebaseFirestore.getInstance();
+        eventsRef = db.collection("Events");
 
+        eventDataArray = new ArrayList<Event>();
+        eventList = findViewById(R.id.event_list);
+        myEventsAdapter = new MyEventsAdapter(this, eventDataArray);
+
+        eventList.setAdapter(myEventsAdapter);
+
+        // Adds listener to event reference. Populates Event Array with Event data from DB
+        eventsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null){
+                    Log.e("FireStore", error.toString());
+                    return;
+                }
+                if (value != null){
+                    eventDataArray.clear();
+                    for (QueryDocumentSnapshot doc : value) {
+                        String title = doc.getString("Title");
+                        Log.d("Firestore: ", String.format("Event (%s) fetched", title) );
+
+                        eventDataArray.add(new Event(title));
+                    }
+                    myEventsAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
--- HotEvents/app/src/main/java/com/example/hotevents/MyEventsAdapter.java
+++ HotEvents/app/src/main/java/com/example/hotevents/MyEventsAdapter.java
@@ -5,12 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
+/**
+ * My Events View Adapter
+ */
 public class MyEventsAdapter extends ArrayAdapter<Event> {
     private ArrayList<Event> myEvents;
     private Context context;
@@ -20,7 +26,7 @@ public class MyEventsAdapter extends ArrayAdapter<Event> {
         this.myEvents = myEvents;
         this.context = context;
     }
-
+    
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
@@ -30,8 +36,14 @@ public class MyEventsAdapter extends ArrayAdapter<Event> {
         } else {
             view = convertView;
         }
+        Event event = myEvents.get(position);
+
+        TextView eventTitle = view.findViewById(R.id.event_title_text);
+        ImageView poster = view.findViewById(R.id.imageView);       // to be implemented
+
+        eventTitle.setText(event.getTitle());
+//        poster.set
 
         return view;
-
     }
 }
