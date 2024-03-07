8334f21969bf6a47ca1dbe40f9dfa696bda40f24
bK0s <bkostely@ualberta.ca>
2024-03-06 19:05:14 -0700

    Revert "Added Users to database"
    This reverts commit f895ded20b297c04027a41a7d585a1aba6308aca.

--- HotEvents/app/src/main/java/com/example/hotevents/MainActivity.java
+++ HotEvents/app/src/main/java/com/example/hotevents/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
     }
         private void handleNewUserInput(FirebaseFirestore db, String deviceId) {
         SignedUpEvent = new ArrayList<String>();
-        UserName = "Harsh Patel";
+        UserName = "Harsh";
         Map<String, Object> newUser = new HashMap<>();
         newUser.put("ProfilePicture", "");
         newUser.put("UID", deviceId);
