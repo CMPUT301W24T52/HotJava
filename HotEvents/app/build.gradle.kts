abd8906f7e61f712f14ecaba430a1273ee68968c
Mahek <mahekkaneria@gmail.com>
2024-04-09 00:41:15 -0600

    Fixed dependencies to solve conflict

--- HotEvents/app/build.gradle.kts
+++ HotEvents/app/build.gradle.kts
@@ -61,16 +61,10 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
 
     implementation("androidx.core:core-splashscreen:1.0.1")
-    testImplementation("org.mockito:mockito-core:3.12.4")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test:runner:1.1.0")
-    androidTestImplementation("androidx.test:rules:1.1.0")
-    androidTestImplementation("androidx.test:core:1.1.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.4.0")
-    testImplementation("org.robolectric:robolectric:4.6.1")
-
+    testImplementation ("org.mockito:mockito-core:3.12.4")
+    androidTestImplementation("androidx.test:rules:1.4.0")
+    androidTestImplementation("com.android.support.test:rules:1.0.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha3")
 }
